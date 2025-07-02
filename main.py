37a4afad0211ba623a79a087e3fc1e3e4e33d11e
osrodgon <osrodgon@gmail.com>
2025-07-02 09:36:55 +0200

    Feature: Added log system to the application.

--- main.py
+++ main.py
@@ -1,25 +1,52 @@
 import logging
+import logging.config
 
 from taximeter import Taximeter
 
 
 def set_log():
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logging_config = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'default': {
+                'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            },
+        },
+        'handlers': {
+            'file': {
+                'level': 'DEBUG',
+                'class': 'logging.FileHandler',
+                'filename': 'taximeter.log',
+                'formatter': 'default',
+            },
+            'stdout': {
+                'level': 'ERROR',
+                'class': 'logging.StreamHandler',
+                'formatter': 'default',
+            },
+        },
+        'loggers': {
+            'Taximeter': {
+                'handlers': ['file', 'stdout'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+        },
+    }
 
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.ERROR)
-    ch.setFormatter(formatter)
+    logging.config.dictConfig(logging_config)
+    my_logger = logging.getLogger('Taximeter')
 
-    logger = logging.getLogger('taximeter')
-    logger.setLevel(logging.ERROR)
-    logger.addHandler(ch)
-
-    return logger
+    return my_logger
 
 if __name__ == '__main__':
     print("Welcome to the taximeter application.\n")
 
     logger = set_log()
+    logger.debug("Initializing the application.")
     taximeter = Taximeter(logger)
 
+    logger.debug("Starting the application.")
     taximeter.run()
+    logger.debug("The application has ended successfully.")
